bbf1ad011a3aa3ffaaa3a54bf445704de4f88657
smithyberry <yeah2109@yeah.net>
2019-08-19 16:26:57 +0800

    rename

--- qunar-demo/src/pages/home/Home.vue
+++ qunar-demo/src/pages/home/Home.vue
@@ -1,35 +1,20 @@
 <template>
   <div class="hello">
-    <div>home</div>
-    <router-link to="/list">list</router-link>
+    <HomeHeader></HomeHeader>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
   name: 'Home',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  components:{
+    HomeHeader
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="stylus" scoped>
+
 </style>
