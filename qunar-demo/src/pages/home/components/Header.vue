4550baa6be83d0c954ef033eeb9494361c8104db
smithyberry <yeah2109@yeah.net>
2019-08-19 17:23:03 +0800

    制作header 配置stylus 配置iconfont

--- qunar-demo/src/pages/home/components/Header.vue
+++ qunar-demo/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
 <div class="header">
-  <div class="header-left">返回</div>
-  <div class="header-input">输入城市/景点/游玩主题</div>
-  <div class="header-right">城市</div>
+  <div class="header-left">
+    <div class="iconfont back-icon">&#xe697;</div>
+  </div>
+  <div class="header-input">
+    <span class="iconfont">&#xe6ac;</span>
+    输入城市/景点/游玩主题
+  </div>
+  <div class="header-right">
+    城市
+    <span class="iconfont arrow-icon">&#xe6a6;</span>
+  </div>
 </div>
 </template>
 <script>
@@ -12,14 +20,19 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" scoped>
+// css中表示src目录用~@
+  @import '~styles/varibles.styl';
   .header
    display:flex
    line-height:0.86rem
-   background #00bcd4
+   background $bgColor
    color #fff
    .header-left
     width:0.64rem
     float:left
+    .back-icon
+     text-align center
+     font-size .5rem
    .header-input
     flex:1
     height .64rem
@@ -29,8 +42,12 @@ export default {
     background #ffffff
     border-radius .1rem
     color #ccc
+    padding-left 0.1rem
    .header-right
       width:1.24rem
       float:right
       text-align center
+      .arrow-icon
+       font-size 0.3rem
+       margin-top 0.1rem
 </style>
