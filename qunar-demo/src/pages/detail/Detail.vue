e2292f6b22d1a1efd1711245a3c82cdeb99094e3
蓝莓铁匠 <yeah2109@yeah.net>
2019-11-26 11:30:41 +0800

    finish detail-list

--- qunar-demo/src/pages/detail/Detail.vue
+++ qunar-demo/src/pages/detail/Detail.vue
@@ -2,17 +2,47 @@
   <div>
     <DetailBanner></DetailBanner>
     <DetailHeader></DetailHeader>
-    <div class="content"></div>
+    <div class="content">
+      <DetailList :list="list"></DetailList>
+    </div>
   </div>
 </template>
 <script>
 import DetailBanner from './components/banner'
 import DetailHeader from './components/header'
+import DetailList from './components/list'
 export default {
   name:'Detail',
   components:{
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data(){
+    return{
+      list:[{
+        title:'成人票',
+        children:[{
+          title:'成人三馆联票',
+          children:[{
+            title:'成人三馆联票-某一连锁店销售'
+          },{
+            title:'成人五馆联票-某一连锁店销售'
+          }
+          ]
+        },{
+          title:'成人五馆联票'
+        }
+        ]
+      },{
+        title:'学生票'
+      },{
+        title:'儿童票'
+      },{
+        title:'特惠票'
+      }
+      ]
+    }
   }
 }
 </script>
