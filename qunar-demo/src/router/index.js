4c474b0c6e0effda4e30bcb774f0193a68fde69b
蓝莓铁匠 <yeah2109@yeah.net>
2019-11-27 17:23:51 +0800

    route 改为异步组件

--- qunar-demo/src/router/index.js
+++ qunar-demo/src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // @代表src目录
-import Home from '@/pages/home/Home.vue'
-import List from '@/pages/list/List.vue'
-import city from '@/pages/city/City.vue'
-import Detail from '@/pages/detail/Detail.vue'
+// import Home from '@/pages/home/Home.vue'
+// import List from '@/pages/list/List.vue'
+// import city from '@/pages/city/City.vue'
+// import Detail from '@/pages/detail/Detail.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -15,25 +15,25 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/Home.vue')
     },
     {
       path: '/list',
       name: 'List',
       // 这里的component名要与import中的名相同
-      component: List
+      component: () => import('@/pages/list/List.vue')
     },
     {
       path: '/city',
       name: 'City',
       // 这里的component名要与import中的名相同
-      component: city
+      component: () => import('@/pages/city/City.vue')
     },
     {
       path: '/detail/:id',
       name: 'Detail',
       // 这里的component名要与import中的名相同
-      component: Detail
+      component: () => import('@/pages/detail/Detail.vue')
     }
   ]
 })
