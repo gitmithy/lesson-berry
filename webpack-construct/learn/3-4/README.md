1b43a334788779bdc45ded787f4e4815be65052b
蓝莓铁匠 <yeah2109@yeah.net>
2019-12-13 16:53:39 +0800

    webpack 打包 typescript

---
+++ webpack-construct/learn/3-4/README.md
@@ -0,0 +1,10 @@
+# 工作以来构建工具学习文档
+
+**webpack打包typescript**
+
+
+###### 1.写ts导入包需要类型声明，可以一开始就 
+###### npm install @types/lodash npm install @types/vue
+###### 或者安装npm install typings 再 typings install lodash --save
+###### 
+
---
+++ webpack-construct/learn/3-4/src/app.ts
@@ -0,0 +1,18 @@
+import * as _ from 'lodash'
+
+console.log(_.chunk([1,2,3,4,5],2))
+
+
+
+const NUM=45
+interface Cat{
+    name:string,
+    gender:string
+}
+function touchCat(cat: Cat){
+    console.log('miao~',cat.name)
+}
+touchCat({
+    name:'tom',
+    gender:'male'
+})
